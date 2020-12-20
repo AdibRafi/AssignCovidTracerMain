0911d5162e25e7b7bdd4dccec3ec6e3922569a1f
Abdjb <63862086+Abdjb@users.noreply.github.com>
2020-12-20 22:14:11 +0800

    test dulu

--- src/test.java
+++ src/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Test je dulu");
+        System.out.println("Kalau ada issue nak run");
+        System.out.println("feel free msg aku");
+        System.out.println("-adib");
     }
 }
