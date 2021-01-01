4fa43a974f6d8734432f3ad4312276b4e3f74d49
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-01-01 21:47:24 +0800

    modify function untuk work for 2d array

--- src/Admin.java
+++ src/Admin.java
@@ -1,59 +1,28 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Admin {
     Admin(){}
-    Scanner readFile;
-    public List<String> getName() throws IOException{
-        File file = new File("customerName");
-        readFile = new Scanner(file);
-        List<String> resultName = new ArrayList<String>();
-        while(readFile.hasNextLine()){
-            String data = readFile.nextLine();
-            resultName.add(data);
-        }
-        return resultName;
-    }
-    public List<String> getPhone() throws IOException{
-        File file = new File("customerPhone");
-        readFile = new Scanner(file);
-        List<String> resultName = new ArrayList<String>();
-        while(readFile.hasNextLine()){
-            String data = readFile.nextLine();
-            resultName.add(data);
-        }
-        return resultName;
-    }
-    public List<String> getShop() throws IOException{
-        File file = new File("shopName");
-        readFile = new Scanner(file);
-        List<String> resultName = new ArrayList<String>();
-        while(readFile.hasNextLine()){
-            String data = readFile.nextLine();
-            resultName.add(data);
-        }
-        return resultName;
-    }
-    public void displayCustomer(String[] name,String[] phone){
+    public void displayCustomer(String[][] strings){
         System.out.println(String.format("%-5s %-15s %-15s %s", "No",
                     "Name", "Phone", "Status"));
-        for (int i = 0; i < name.length; i++) {
+        for (int i = 0; i < strings.length; i++) {
             System.out.println(String.format("%-5s %-15s %-15s %s",
-                    i, name[i], phone[i], "Normal"));
+                    i, strings[i][0], strings[i][1],strings[i][2]));
         }
     }
-    public void displayShops(String[] name,String[] phone, String[] shop){
+    public void displayShops(String[][] strings){
         System.out.println(String.format("%-5s %-15s %-15s %-10s %s","No",
                 "Name", "Phone", "Manager","Status"));
-        for (int i = 0; i <name.length; i++) {
+        for (int i = 0; i <strings.length; i++) {
             System.out.println(String.format("%-5s %-15s %-15s %-10s %s"
-                    , i,shop[i],phone[i],name[i],"Normal"));
+                    , i,strings[i][0],strings[i][1],strings[i][2],strings[i][3]));
         }
     }
-
-
-
+    public void displayMaster(String[][] strings){
+        System.out.println(String.format("%-5s %-13s %-10s %-16s %s","No",
+                "Date", "Time", "Customer","Shop"));
+        for (int i = 0; i <strings.length; i++) {
+            System.out.println(String.format("%-5s %-13s %-10s %-16s %s"
+                    , i,strings[i][0],strings[i][1],strings[i][2],strings[i][3]));
+        }
+    }
 }
+
--- src/adminTest.java
+++ src/adminTest.java
@@ -1,29 +1,39 @@
 import java.io.IOException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Timer;
 
 public class adminTest {
     public static void main(String[] args) throws IOException {
         System.out.println("1. List of Customers");
         System.out.println("2. List of Shops");
+        System.out.println("3. Master");
         System.out.print("Press the number = ");
+
 
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
 
-        Admin x = new Admin();
-        String[] adminName = x.getName().toArray(new String[0]);
-        String[] adminPhone = x.getPhone().toArray(new String[0]);
-        String[] adminShop = x.getShop().toArray(new String[0]);
+        fileStuff x = new fileStuff();
+        String[][] customertest = x.getFileReading("CustomerFileAdmin");
+        String[][] shoptest = x.getFileReading("ShopFileAdmin");
+        String[][] mastertest= x.getFileReading("MasterFileAdmin");
+        String[][] test =  {{"adib","3"},
+                            {"darwisy","5"},
+                            {"ahmad","6"},
+                {"Azri","10"},
+                {"Abu","23"}};
 
-        if (userInput.equals("1")) {
-            x.displayCustomer(adminName,adminPhone);
+        x.fileWriting("MEKDIBUTO",test);
 
-        } else if (userInput.equals("2")) {
-            x.displayShops(adminName,adminPhone,adminShop);
-        } else {
-            System.out.println("Input Error... try again");
+        Admin y = new Admin();
+        switch (userInput) {
+            case "1" -> y.displayCustomer(customertest);
+            case "2" -> y.displayShops(shoptest);
+            case "3" -> y.displayMaster(mastertest);
+            default -> System.out.println("Input Error... try again");
         }
 
     }
