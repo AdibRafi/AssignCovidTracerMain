d3aae8e432f2e0be186efcf6b5c308fe68181073
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-01-01 16:20:53 +0800

    .

--- src/testingGroundAdib.java
+++ src/testingGroundAdib.java
@@ -1,18 +1,23 @@
-class Person {
-
-    String name;
-    int age;
-
-}
-
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class testingGroundAdib {
-
-    public static void main(String[] args) {
-
-        System.out.println(String.format("%s %10s %10s","foo","bar","baz"));
-        System.out.println(String.format("%s %10s %10s","foofoo","barbar","bazbaz"));
-
+    public static void main(String[] args) throws IOException {
+//        File file = new File("customerName");
+//        Scanner readFile = new Scanner(file);
+//
+//        List<String> temps = new ArrayList<String>();
+//
+//        while (readFile.hasNextLine()){
+//            String data = readFile.nextLine();
+//            temps.add(data);
+//        }
+//        System.out.println(temps);
+        Admin x = new Admin();
+//        String[] test = x.getName().toArray(new String[0]);
+//        System.out.println(test[0]);
     }
-
 }
