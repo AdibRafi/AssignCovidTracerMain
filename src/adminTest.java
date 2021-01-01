a251b6e44f66c5f13b2ef373adc29e155b13ff20
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-01-01 12:17:14 +0800

    modified

--- src/adminTest.java
+++ src/adminTest.java
@@ -1,9 +1,10 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class adminTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("1. List of Customers");
         System.out.println("2. List of Shops");
         System.out.print("Press the number = ");
@@ -11,32 +12,18 @@ public class adminTest {
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
 
-        // TESTING ARRAY
-        String[] adminName = {"Ali", "Adib", "Darwisy"};
-        long[] adminPhone = {0103442345, 324523543, 435645345};
-        String[] adminShop={"Tesco", "Giant" , "Jaya Grocer"};
-
-
-
         Admin x = new Admin();
-        if (userInput.equals("1")) {
-            System.out.println(String.format("%-5s %-15s %-15s %s", "No",
-                    "Name", "Phone", "Status"));
-            for (int i = 0; i < adminName.length; i++) {
-                System.out.println(String.format("%-5s %-15s %-15s %s",
-                        i, adminName[i], adminPhone[i], "Normal"));
-            }
-        } else if (userInput.equals("2")) {
-            System.out.println(String.format("%-5s %-15s %-15s %-10s %s","No",
-                    "Name", "Phone", "Manager","Status"));
-            for (int i = 0; i <adminName.length; i++) {
-                System.out.println(String.format("%-5s %-15s %-15s %-10s %s"
-                , i,adminShop[i],adminPhone[i],adminName[i],"Normal"));
-            }
+        String[] adminName = x.getName().toArray(new String[0]);
+        String[] adminPhone = x.getPhone().toArray(new String[0]);
+        String[] adminShop = x.getShop().toArray(new String[0]);
 
+        if (userInput.equals("1")) {
+            x.displayCustomer(adminName,adminPhone);
+
+        } else if (userInput.equals("2")) {
+            x.displayShops(adminName,adminPhone,adminShop);
         } else {
             System.out.println("Input Error... try again");
-
         }
 
     }
