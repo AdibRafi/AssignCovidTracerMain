d2d2ad3f4e06f1a658d413a2ab2a8ab360c9c52c
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-01-01 21:47:45 +0800

    adding reading n writing funciton untuk kegunaan semua

--- src/fileStuff.java
+++ src/fileStuff.java
@@ -1,28 +1,53 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class fileStuff {
-
-    Scanner readFile;
-    public List<String> readingFile(String fileName) throws IOException{
-        File file = new File(fileName);
-        readFile = new Scanner(file);
-        List<String> result = new ArrayList<String>();
-        while(readFile.hasNextLine()){
-            String data = readFile.nextLine();
-            result.add(data);
-        }
-        return result;
-    }
-    // [firstname,lastname,password,phoneNo.]
-    public void fileWriting(String fileName,String arr)throws IOException{
+    public void fileWriting(String fileName,String arr)throws IOException {
         FileWriter file = new FileWriter(fileName);
         file.write(arr);
         file.close();
     }
+    public String[][] getFileReading(String fileName) throws IOException{
+        Scanner input = new Scanner(new File(fileName));
+        List<String[]> lines = new ArrayList<>();
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            String[] lineSplit = line.split(",");
+            lines.add(lineSplit);
+        }
+        String[][] result = new String[lines.size()][];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = lines.get(i);
+        }
+        return result;
+    }
+    public void fileWriting(String filename, String[][] strings) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length; j++) {
+                writer.write(strings[i][j]+
+                        ((j==strings[i].length-1) ? "" : ","));
+            }
+            writer.newLine();
+        }
+        writer.flush();
+    }
 }
+
+// ARCHIVE
+// Scanner readFile;
+//    public List<String> readingFile(String fileName) throws IOException{
+//        File file = new File(fileName);
+//        readFile = new Scanner(file);
+//        List<String> result = new ArrayList<String>();
+//        while(readFile.hasNextLine()){
+//            String data = readFile.nextLine();
+//            result.add(data);
+//        }
+//        return result;
+//    }
