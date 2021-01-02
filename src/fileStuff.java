0d11a9114e26c64c2596b5223249563e70760f1a
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-01-02 10:42:33 +0800

    update punya

--- src/fileStuff.java
+++ src/fileStuff.java
@@ -1,18 +1,16 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class fileStuff {
-    public void fileWriting(String fileName,String arr)throws IOException {
-        FileWriter file = new FileWriter(fileName);
-        file.write(arr);
-        file.close();
+    private String fileName;
+    fileStuff(){}
+    fileStuff(String fileName){this.fileName = fileName;}
+    public void fileWriting(String arr)throws IOException {
+        FileWriter writer = new FileWriter(fileName);
+        writer.write(arr);
+        writer.close();
     }
-    public String[][] getFileReading(String fileName) throws IOException{
+    public String[][] getFileReading() throws IOException{
         Scanner input = new Scanner(new File(fileName));
         List<String[]> lines = new ArrayList<>();
         while (input.hasNextLine()){
@@ -26,8 +24,8 @@ public class fileStuff {
         }
         return result;
     }
-    public void fileWriting(String filename, String[][] strings) throws IOException{
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+    public void fileWriting(String[][] strings) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
         for (int i = 0; i < strings.length; i++) {
             for (int j = 0; j < strings[i].length; j++) {
                 writer.write(strings[i][j]+
@@ -37,7 +35,39 @@ public class fileStuff {
         }
         writer.flush();
     }
+    public void sortDate()throws IOException{
+        fileStuff f = new fileStuff();
+        String[][] input = f.getFileReading(fileName);
+        Scanner inputSys = new Scanner(System.in);
+        String inputUser = inputSys.nextLine();
+        System.out.println("Which column do you want to sort?");
+
+        switch (inputUser){
+            case "1" -> Arrays.sort(input,Comparator.comparing(o -> o[0]));
+            case "2" -> Arrays.sort(input,Comparator.comparing(o -> o[1]));
+            default -> System.out.println("Input error");
+        }
+        System.out.println("dah");
+        System.out.println(Arrays.deepToString(input));
+
+    }
+
+    private String[][] getFileReading(String fileName) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(this.fileName = fileName));
+        List<String[]> lines = new ArrayList<>();
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            String[] lineSplit = line.split(",");
+            lines.add(lineSplit);
+        }
+        String[][] result = new String[lines.size()][];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = lines.get(i);
+        }
+        return result;
+    }
 }
+
 
 // ARCHIVE
 // Scanner readFile;
