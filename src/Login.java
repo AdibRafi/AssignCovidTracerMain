46654d80e67fe074efe84d9ecdd16076f94a8ca4
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-01-07 16:43:04 +0800

    login update

--- src/Login.java
+++ src/Login.java
@@ -4,20 +4,30 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Login extends Registration {
-    private String userName;
-    private String password;
-    fileStuff f = new fileStuff("registerStuff");
-    String[] infoThing = f.getFileReading1row();
+    private String loginUserName;
+    private String loginPassword;
+    public static void main(String[] args) throws IOException {
+        Login l = new Login();
+        l.startLogin();
+    }
 
     public Login() throws IOException {}
 
+    public void setUserName(String userName) {
+        this.loginUserName = userName;
+    }
+    public void setPassword(String password) {
+        this.loginPassword = password;
+    }
 
-    public String getUserName() {
-        return infoThing[0] + infoThing[1];
+    public String getLoginUserName() {
+        return loginUserName;
     }
-    public String getPassword(){
-        return infoThing[2];
+
+    public String getLoginPassword() {
+        return loginPassword;
     }
+
     public void startLogin() throws IOException {
         Scanner input = new Scanner(System.in);
         Customer custSelect = new Customer();
@@ -32,16 +42,35 @@ public class Login extends Registration {
         String[][] inputFromFile = f.getFileReading();
         int loop = 0;
 
-        for (int i = 0; i < inputFromFile.length; i++) {
-            if (userName.equals(inputFromFile[i][0]+inputFromFile[i][1])
-            &&password.equals(inputFromFile[i][2])){
+        try {
+            for (int i = 0; i < inputFromFile.length; i++) {
+                if (userName.equals(inputFromFile[i][0] + inputFromFile[i][1])
+                        && password.equals(inputFromFile[i][2])) {
+                    System.out.println("User successfully logined...");
+                    custSelect.custMenuSelect();
+                    setUserName(userName);
+                    setPassword(password);
+                    System.out.println(getLoginUserName());
+                    System.out.println(getLoginPassword());
+
+                    loop++;
+                }
+            }
+            if (loop == 0)
+                System.out.println("Invalid username or password.");
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            if (userName.equals(inputFromFile[0][0] + inputFromFile[0][1])
+                    && password.equals(inputFromFile[0][2])) {
                 System.out.println("User successfully logined...");
                 custSelect.custMenuSelect();
-                loop++;
+                setUserName(userName);
+                setPassword(password);
+            }
+            else{
+                System.out.println("Invalid UserName or Password");
             }
         }
-        if (loop == 0)
-            System.out.println("Invalid username or password.");
 
 
 //        if(userName.equals(info.getUserName()) && password.equals(info.getPassword())){
